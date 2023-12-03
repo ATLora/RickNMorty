1cde78640e8371866604f85b44523724c32564fb
David Lora <lora.david2096@gmail.com>
2023-12-03 01:29:58 -0500

    Working on Fav style

--- client/src/Views/Favorites.jsx
+++ client/src/Views/Favorites.jsx
@@ -18,31 +18,35 @@ const Favorites = (props) => {
   };
 
   return (
-    <div>
-      <select onChange={handleOrder}>
-        <option value="A">Ascendente</option>
-        <option value="D">Descendente</option>
-      </select>
+    <div className="pt-8 w-full min-h-screen bg-blue-900">
+      <div>
+        <select onChange={handleOrder}>
+          <option value="A">Ascendente</option>
+          <option value="D">Descendente</option>
+        </select>
 
-      <select onChange={handleFilter}>
-        <option value="Male">Male</option>
-        <option value="Female">Female</option>
-        <option value="Genderless">Genderless</option>
-        <option value="unknown">unknown</option>
-      </select>
-      {myFavorites.map((char) => {
-        return (
-          <Card
-            key={char.id}
-            id={char.id}
-            name={char.name}
-            species={char.species}
-            gender={char.gender}
-            image={char.image}
-            status={char.status}
-          />
-        );
-      })}
+        <select onChange={handleFilter}>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Genderless">Genderless</option>
+          <option value="unknown">unknown</option>
+        </select>
+      </div>
+      <div className="pt-8 grid gap-2 sm:gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+        {myFavorites.map((char) => {
+          return (
+            <Card
+              key={char.id}
+              id={char.id}
+              name={char.name}
+              species={char.species}
+              gender={char.gender}
+              image={char.image}
+              status={char.status}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
